90ea983cb014e98aeb966aeb56601ec25fff9d89
FreddieD <>
2020-08-19 18:20:07 +0200

    working deploy version

--- multicoreIcosaledron/multicoreIcosaledron.ino
+++ multicoreIcosaledron/multicoreIcosaledron.ino
@@ -63,13 +63,16 @@ Input vectors receive computed results from FFT
 //wifi stuff
 WiFiServer server(80);
 
+String header;
+String otaMode = "off";
+String lightMode = "off";
 
-String responseHTML = ""
-  "<!DOCTYPE html><html><head><title>CaptivePortal</title></head><body>"
-  "<h1>Hello World!</h1><p>This is a captive portal example. All requests will "
-  "be redirected here.</p></body></html>";
-
-
+// Current time
+unsigned long currentTime = millis();
+// Previous time
+unsigned long previousTime = 0; 
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+const long timeoutTime = 2000;
 
 void setup() {
   delay(3000); // 3 second delay for recovery
@@ -147,7 +150,6 @@ void setup() {
 }
 
 //Task1code: blinks an LED every 1000 ms
-int otaMode = 0;
 int runcount = 0;
 void Task1code( void * pvParameters ){
   // core0 loop goes here network interaction
@@ -156,35 +158,100 @@ void Task1code( void * pvParameters ){
     knob2Val = analogRead(Knob2);
   delay(500);
   Serial.println("running core 0");
-    if ((knob1Val == 4095 && knob2Val == 4095) || otaMode == 1) {
+    if ((knob1Val == 4095 && knob2Val == 4095) || otaMode == "on") {
        ArduinoOTA.handle();
     } else {
       
-      // run the server
-      //dnsServer.processNextRequest();
-      WiFiClient client = server.available();   // listen for incoming clients
+      WiFiClient client = server.available();   // Listen for incoming clients
 
-      if (client) {
-        String currentLine = "";
-        while (client.connected()) {
-          if (client.available()) {
-            char c = client.read();
-            if (c == '\n') {
+      if (client) {                             // If a new client connects,
+        Serial.println("New Client.");          // print a message out in the serial port
+        String currentLine = "";                // make a String to hold incoming data from the client
+        currentTime = millis();
+        previousTime = currentTime;
+        while (client.connected() && currentTime - previousTime <= timeoutTime) { // loop while the client's connected
+          currentTime = millis();         
+          if (client.available()) {             // if there's bytes to read from the client,
+            char c = client.read();             // read a byte, then
+            Serial.write(c);                    // print it out the serial monitor
+            header += c;
+            if (c == '\n') {                    // if the byte is a newline character
+              // if the current line is blank, you got two newline characters in a row.
+              // that's the end of the client HTTP request, so send a response:
               if (currentLine.length() == 0) {
+                // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+                // and a content-type so the client knows what's coming, then a blank line:
                 client.println("HTTP/1.1 200 OK");
                 client.println("Content-type:text/html");
+                client.println("Connection: close");
                 client.println();
-                client.print(responseHTML);
+                
+                // turns the GPIOs on and off
+                if (header.indexOf("GET /1/on") >= 0) {
+                  Serial.println("GPIO 5 on");
+                  otaMode = "on";
+                } else if (header.indexOf("GET /1/off") >= 0) {
+                  Serial.println("GPIO 5 off");
+                  otaMode = "off";
+                } else if (header.indexOf("GET /2/on") >= 0) {
+                  Serial.println("GPIO 4 on");
+                  lightMode = "on";
+                } else if (header.indexOf("GET /2/off") >= 0) {
+                  Serial.println("GPIO 4 off");
+                  lightMode = "off";
+                }
+                
+                // Display the HTML web page
+                client.println("<!DOCTYPE html><html>");
+                client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+                client.println("<link rel=\"icon\" href=\"data:,\">");
+                // CSS to style the on/off buttons 
+                // Feel free to change the background-color and font-size attributes to fit your preferences
+                client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+                client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
+                client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+                client.println(".button2 {background-color: #77878A;}</style></head>");
+                
+                // Web Page Heading
+                client.println("<body><h1>IcosaLEDron Web Server</h1>");
+                
+                // Display current state, and ON/OFF buttons for GPIO 5  
+                client.println("<p>OTAMode - State " + otaMode + "</p>");
+                // If the otaMode is off, it displays the ON button       
+                if (otaMode=="off") {
+                  client.println("<p><a href=\"/1/on\"><button class=\"button\">ON</button></a></p>");
+                } else {
+                  client.println("<p><a href=\"/1/off\"><button class=\"button button2\">OFF</button></a></p>");
+                } 
+                   
+                // Display current state, and ON/OFF buttons for GPIO 4  
+                client.println("<p>Lights - State " + lightMode + "</p>");
+                // If the lightMode is off, it displays the ON button       
+                if (lightMode=="off") {
+                  client.println("<p><a href=\"/2/on\"><button class=\"button\">ON</button></a></p>");
+                } else {
+                  client.println("<p><a href=\"/2/off\"><button class=\"button button2\">OFF</button></a></p>");
+                }
+                client.println("</body></html>");
+                
+                // The HTTP response ends with another blank line
+                client.println();
+                // Break out of the while loop
                 break;
-              } else {
+              } else { // if you got a newline, then clear currentLine
                 currentLine = "";
               }
-            } else if (c != '\r') {
-              currentLine += c;
+            } else if (c != '\r') {  // if you got anything else but a carriage return character,
+              currentLine += c;      // add it to the end of the currentLine
             }
           }
         }
+        // Clear the header variable
+        header = "";
+        // Close the connection
         client.stop();
+        Serial.println("Client disconnected.");
+        Serial.println("");
       }
     }
   } 
@@ -193,8 +260,20 @@ void Task1code( void * pvParameters ){
 //Task2code: blinks an LED every 700 ms
 void Task2code( void * pvParameters ){
   // core1 loop goes here flashy lights bits
+
   for(;;){
-    runningLight();
+    
+    //knob1Val = analogRead(Knob1);
+    //knob2Val = analogRead(Knob2);
+    if (otaMode == "on") { // check for ota}
+      greenwashfade();
+    } else {
+      if (lightMode == "off") {
+        runningLight();
+      } else {
+        blackOut();
+      }
+    }
   }
 }
 
@@ -224,4 +303,32 @@ void runningLight() {
     }
     s = 255;
     v = 250;
+}
+
+void greenwashfade(){
+  for (int j = 0; j < 255; j++) {
+    
+    for( int i = 0; i < NUM_LEDS; i++) { //9948
+      leds[i] = CHSV(100, 255, j);
+    }
+    FastLED.show();
+    delay(1);
+  }
+  
+  for (int j = 255; j > 100; j--) {
+    
+    for( int i = 0; i < NUM_LEDS; i++) { //9948
+      leds[i] = CHSV(100, 255, j);
+    }
+    FastLED.show();
+    delay(1);
+  }
+}
+
+void blackOut() {
+  for(int dot = 0; dot < NUM_LEDS; dot++) { 
+    leds[dot] = CHSV(0, 0, 100);
+    delay(1);
+    FastLED.show();
+  }
 }
